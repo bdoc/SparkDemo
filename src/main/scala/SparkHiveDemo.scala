b0594fc4ab9ec27248055a9a13b7684a9892f75b
Eber H <ebcead@outlook.com>
2018-06-07 15:06:17 +0800

    add ignore

---
+++ src/main/scala/SparkHiveDemo.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkHiveDemo {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Spark Hive Demo")
+    val sc = new SparkContext(conf)
+    val sqlContext = new HiveContext(sc)
+    sqlContext.sql("show databases").show()
+  }
+}
