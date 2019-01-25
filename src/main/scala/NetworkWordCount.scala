89e5e492495ab0fe490fb592b69591d5952f89a5
Tobyhong <tobyhong@lagou.com>
2019-01-25 12:17:07 +0800

    NetworkWordCount (nc -lk 9999)

---
+++ src/main/scala/NetworkWordCount.scala
@@ -0,0 +1,24 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds,StreamingContext}
+
+object NetworkWordCount {
+  def main(args: Array[String]): Unit = {
+    if (args.length < 2) {
+      System.err.println("Usage: NetworkWordCount <hostname> <port>")
+      System.exit(1)
+    }
+
+    val sparkConf = new SparkConf().setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(1))
+
+    val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_AND_DISK_SER)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(word => (word,1)).reduceByKey(_ + _)
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+
+}
